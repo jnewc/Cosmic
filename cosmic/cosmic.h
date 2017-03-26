af4986166eb282fb9ecb21fa8338c35667b3b276
Jack Newcombe <jack@newcombe.io>
2017-03-26 20:56:35 +0100

    Initial Commit

---
+++ cosmic/cosmic.h
@@ -0,0 +1,19 @@
+//
+//  cosmic.h
+//  cosmic
+//
+//  Created by Jack Newcombe on 26/03/2017.
+//  Copyright © 2017 Jack Newcombe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cosmic.
+FOUNDATION_EXPORT double cosmicVersionNumber;
+
+//! Project version string for cosmic.
+FOUNDATION_EXPORT const unsigned char cosmicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cosmic/PublicHeader.h>
+
+
