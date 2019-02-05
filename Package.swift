ec2018779aee4b2255e3998cb0335ee1110096b1
Jack Newcombe <jack@newcombe.io>
2019-02-05 19:18:34 +0000

    Fixes for swift 4.2 + SPM

--- Package.swift
+++ Package.swift
@@ -1,8 +1,22 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "Cosmic",
 	dependencies: [
-		.Package(url: "https://github.com/IBM-Swift/BlueSocket", majorVersion: 1, minor: 0)
-	]
+		.package(url: "https://github.com/IBM-Swift/BlueSocket", from: "1.0.0")
+	],
+  targets: [
+    .target(
+      name: "Cosmic",
+      dependencies: [
+        "Socket"
+      ]
+    ),
+    .testTarget(
+      name: "CosmicTests",
+      dependencies: [ "Cosmic" ]
+    )
+  ],
+  swiftLanguageVersions: [.v4_2]
 )
--- Sources/Cosmic/Formatters/JSONFormatter.swift
+++ Sources/Cosmic/Formatters/JSONFormatter.swift
@@ -15,55 +15,59 @@ typealias JSONFormatterDictionary = [String: Any]
 typealias JSONFormatterConverterCompletion = (String) -> JSONFormatterDictionary
 
 protocol JSONFormatterConverter {
-    
+
     /// <#Description#>
     var converter: JSONFormatterConverterCompletion { get }
-    
+
     func toJSON(message: String, metadata: LogMetadata) -> String?
-    
+
 }
 
 
 /// Converts a string-based textual message into string-based JSON
 class JSONFormatter: LogFormatter, JSONFormatterConverter  {
-    
+
     let converter: JSONFormatterConverterCompletion
-    
-    var options: JSONSerialization.WritingOptions = []
-    
+
+    var options: JSONSerialization.WritingOptions = {
+      if #available(macOS 10.13, *) {
+        return [ .sortedKeys ]
+      }
+      return []
+    }()
     init(converter: @escaping JSONFormatterConverterCompletion) {
         self.converter = converter
     }
-    
+
     func toJSON(message: String, metadata: LogMetadata) -> String? {
         let dict = self.converter(message)
         return JSONSerialization.string(withJSONObject: dict, options: options)
     }
-    
+
     func format(message: String, metadata: LogMetadata) -> String {
         guard let string = toJSON(message: message, metadata: metadata) else { return message }
         return string
     }
-    
-    
+
+
 }
 
 // MARK: NewLineBatchFormatter
 
 public class JSONBatchFormatter: BatchFormatter {
-    
+
     private let jsonFormatter: JSONFormatter
-    
+
     init(converter: @escaping JSONFormatterConverterCompletion) {
         jsonFormatter = JSONFormatter(converter: converter)
     }
-    
+
     public func format(message: String, metadata: LogMetadata) -> String {
         return jsonFormatter.format(message: message, metadata: metadata)
     }
-    
+
     public func format(batch: [(String, LogMetadata)]) -> String {
         return batch.map({ format(message: $0.0, metadata: $0.1) }).joined(separator: "\n")
     }
-    
+
 }
