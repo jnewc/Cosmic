84e9246202bdd505b5c792a39a62ece4db8d4519
Jack Newcombe <jack@newcombe.io>
2019-04-05 20:53:50 +0100

    Fixes

--- Tests/CosmicTests/UniversalSocket.swift
+++ Tests/CosmicTests/UniversalSocket.swift
@@ -10,33 +10,27 @@ import XCTest
 @testable import Cosmic
 
 class UniversalSocketTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testSocketSetup() {
-        
+
         let config = SocketLoggerConfig(host: "", port: 8084, transport: .tcp)
-        
+
         let socket = try! UniversalSocket(config: config)
         try! socket.listen()
-        
+
         XCTAssert(socket.socket.isListening)
         XCTAssertEqual(socket.socket.remoteHostname, "0.0.0.0")
-<<<<<<< Updated upstream
         XCTAssertEqual(socket.socket.listeningPort, 8084)
-=======
-        XCTAssertEqual(socket.socket.listeningPort, 8080)
-        
-        socket.close()
->>>>>>> Stashed changes
     }
-    
+
 }
